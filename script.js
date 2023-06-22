2a1f22443fad48ceee03886619e0c44fd162fcec
Andor <andorhorvat@gmail.com>
2023-06-22 15:35:58 +0200

    Small corrections.

--- script.js
+++ script.js
@@ -14,4 +14,38 @@ listItem.innerHTML = 'Updated Item';
 // console.log('3', document.querySelector('#content'));  // Log a specific element by its ID
 // console.log('4', document.title);  // Log the title of the document
 
+//Using the BOM (Windows) object
+// console.dir(window);
+// console.dir(window.location);
+// console.dir(window.history);
+// console.dir(window.screen);
 
+//Using the screen object
+// console.log("Available Height: " + screen.availHeight);
+// console.log("Total Height: " + screen.height);
+// console.log("Available Width: " + screen.availWidth);
+// console.log("Total Width: " + screen.width);
+
+//Using the navigator object
+// console.log('navigator:', navigator);
+
+//Iterating through all navigator properties
+// for (var prop in navigator) {
+//     console.dir(navigator[prop]);
+// }
+
+document.getElementById("go-forward").addEventListener("click", (event) => {
+    console.log('button forward clicked');
+    console.log('event', event);
+    history.forward();
+});
+
+document.getElementById("go-back").addEventListener("click", () => {
+    console.log('button back clicked');
+    history.back();
+});
+
+
+
+  
+  
